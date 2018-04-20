638bc9bf110b873af9579ffbf3cb45a5b6c3543c
luoyin <luoyin1986@gmail.com>
2018-04-20 17:01:38 +0800

    Update cpu_forward

--- MCS8/iverilog-PIPE/cpu_forward.v
+++ MCS8/iverilog-PIPE/cpu_forward.v
@@ -79,6 +79,14 @@ module cpu_forward(
 	// **********
 	assign wM_Enable = (~(|(M_DST_I ^ REG_SRC_I))) & M_DSTR_CS_I & M_VALID_I;
 	assign wW_Enable = (~(|(W_DST_I ^ REG_SRC_I))) & W_DSTR_CS_I & W_VALID_I;
+	assign REG_BANK_O = ( {8{(~wM_Enable)&(~wW_Enable)}} & REG_BANK_I ) |
+						( {8{( wM_Enable)&(M_DSTR_CS_C_I)}} & M_VAL_C_I ) |
+						( {8{( wM_Enable)&(M_DSTR_CS_S_I)}} & M_VAL_S_I ) |
+						( {8{( wM_Enable)&(M_DSTR_CS_E_I)}} & M_VAL_E_I ) |
+						( {8{(~wM_Enable)&( wW_Enable)&(W_DSTR_CS_C_I)}} & W_VAL_C_I ) |
+						( {8{(~wM_Enable)&( wW_Enable)&(W_DSTR_CS_S_I)}} & W_VAL_S_I ) |
+						( {8{(~wM_Enable)&( wW_Enable)&(W_DSTR_CS_E_I)}} & W_VAL_E_I ) |
+						( {8{(~wM_Enable)&( wW_Enable)&(W_DSTR_CS_M_I)}} & W_VAL_M_I );
 
 	// **********
 	// MAIN CODE
